f0ff3e0edaa01f12f95560e69322cd5d92507297
DellBoy <mlagan06@gmail.com>
2020-10-15 14:27:08 +0100

    Add project files.

---
+++ ScrollApp2/ScrollApp2/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ScrollApp2.Models;
+
+namespace ScrollApp2.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
